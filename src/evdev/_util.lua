c1af409151ca52b08685ca78c707f0085ad67fa1
Haitham <hmmelsm@gmail.com>
2026-05-24 04:05:43 +0300

    feat: add shared Lua helpers

---
+++ src/evdev/_util.lua
@@ -0,0 +1,30 @@
+local fmt = string.format
+
+local M = {}
+
+function M.tbl_copy(input)
+  local output = {}
+  for k, v in pairs(input) do
+    output[k] = v
+  end
+  return output
+end
+
+function M.tbl_keys(t)
+  local keys = {}
+  for k in pairs(t) do
+    keys[#keys + 1] = k
+  end
+  return keys
+end
+
+---@type fun <T> (lbl:string?, v:any, tp:type, optional:boolean?):(value:T)
+function M.validate(lbl, v, tp, optional)
+  local tv = type(v)
+  if tv == tp or (optional and v == nil) then
+    return v
+  end
+  error(fmt("%s: (%s expected, got %s)", lbl, tp, tv), 2)
+end
+
+return M
