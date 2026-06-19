da8b4850a624cfe50eecad39cf02fa14fe69127b
Haitham <hmmelsm@gmail.com>
2026-06-19 22:50:56 +0300

    chore: add version variable to evdev Lua bindings

--- types/evdev.d.lua
+++ types/evdev.d.lua
@@ -4,11 +4,12 @@
 ---@alias evdev.path string Path to an evdev device node or related input path.
 ---@alias evdev.eventValue integer Numeric value attached to an input event.
 
+local version = "evdev 0.2.0" -- x-release-please-version
+
 ---
 ---Lua bindings for Linux evdev devices and /dev/uinput virtual devices.
 ---
 ---@class evdev
----@field _VERSION "evdev 0.2.0"
 ---@field _core evdev._core
 ---@field device evdev.device
 ---@field devices evdev.devices
@@ -17,5 +18,6 @@
 ---@field selector evdev.selector
 ---@field uinput evdev.uinput
 return {
+  _VERSION = version,
   _util = {}, ---@module "evdev._util"
 }
